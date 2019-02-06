160312b51d09473d59330a1b250a030b05a3e35d
hariprasath95@gmail.com <hariprasath95>
2019-02-06 14:54:31 -0500

    JSql Parser

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -1,7 +1,16 @@
 package dubstep;
 
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.JSqlParser;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.sql.Statement;
+
 public class Main {
+
+    StringReader input = new StringReader("SELECT * FROM R");
 
 
 }
