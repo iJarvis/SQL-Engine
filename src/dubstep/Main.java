96ae6d33e11d8a534c00f771cb4a586cec0077da
hariprasath95@gmail.com <hariprasath95>
2019-02-06 15:42:00 -0500

    PA1 - Crud

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -1,13 +1,52 @@
 package dubstep;
 
 
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.JSqlParser;
+import net.sf.jsqlparser.parser.ParseException;
+import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectBody;
+
+import javax.management.Query;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String expr = scanner.next();
+        CCJSqlParser parser = new CCJSqlParser(new StringReader(expr));
+        Statement query = parser.Statement();
+        if(query instanceof Select)
+        {
+            PlainSelect plainSelect = (PlainSelect)query;
+            Table table =(Table) plainSelect.getFromItem();
+            String table_name = table.getName();
+            BufferedReader br = new BufferedReader(new FileReader("data/"+table_name+".csv"));
+            try {
+                String line = br.readLine();
+
+                while (line != null) {
+                    System.out.println(line);
+                    line = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                br.close();
+            }
+        }
+        else
+        {
+            System.out.println("Unknown statement");
+        }
+
 
     }
 
