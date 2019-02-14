647e94d7abf762b96a30f38864ccfb50e3e1111e
hariprasath95 <hariprasath95@gmail.com>
2019-02-14 15:59:29 -0500

    Planner skeleton

---
+++ src/dubstep/planner/PlanTree.java
@@ -0,0 +1,21 @@
+package dubstep.planner;
+
+import dubstep.executor.BaseNode;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+
+public class PlanTree {
+
+    public BaseNode GeneratePlan(PlainSelect plainSelect)
+    {
+        return null;
+    }
+
+    public BaseNode OptimizePlan(BaseNode generatedPlan)
+    {
+        BaseNode optimizedPlan = null;
+        return optimizedPlan;
+    }
+
+
+
+}
