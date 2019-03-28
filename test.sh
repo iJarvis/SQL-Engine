6b127804a0b1671d9cbd3020d57d0bf8b126a408
Mohammad Umair <omerjerk@gmail.com>
2019-03-27 22:17:05 -0400

    add option to pass in-mem/on-disk flag
    Signed-off-by: Mohammad Umair <omerjerk@gmail.com>

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 mkdir -p out/test
 find -name "*.java" > sources.txt
 javac -d out/test -cp libs/jsqlparser-1.0.0.jar:libs/evallib-1.0.jar @sources.txt
-java -classpath out/test:libs/jsqlparser-1.0.0.jar:libs/evallib-1.0.jar dubstep.Main < $1
+java -classpath out/test:libs/jsqlparser-1.0.0.jar:libs/evallib-1.0.jar dubstep.Main $2 < $1
