f69a199dbb595f274958a7307de18a5ca781f069
hariprasath95 <hariprasath95@gmail.com>
2019-03-31 00:16:03 -0400

    disable explain

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -25,7 +25,7 @@ public class Main {
     // Globals used across project
     static public int maxThread = 1;
     static public boolean DEBUG_MODE = false; // will print out logs - all logs should be routed through this flag
-    static public boolean EXPLAIN_MODE = true; // will print statistics of the code
+    static public boolean EXPLAIN_MODE = false; // will print statistics of the code
     static public int SCAN_BUFER_SIZE = 100; //  number of rows cached per scan from disk
 
     public static void main(String[] args) throws ParseException, SQLException {
