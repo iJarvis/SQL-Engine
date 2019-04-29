6e5515431392bd38ee514cc605c841a981b57de8
hariprasath95 <hariprasath95@gmail.com>
2019-04-29 09:39:48 -0400

    fixes

--- src/dubstep/executor/LimitNode.java
+++ src/dubstep/executor/LimitNode.java
@@ -30,6 +30,7 @@ public class LimitNode extends BaseNode {
 
     @Override
     public void initProjPushDownInfo() {
+        if(this.parentNode !=null)
         this.requiredList = this.parentNode.requiredList;
         this.innerNode.initProjPushDownInfo();
     }
