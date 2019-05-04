b067c3cbe12cfbff80e0c6c4e5d0903c763c4f0d
hariprasath95 <hariprasath95@gmail.com>
2019-05-03 20:44:52 -0400

    fix

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -157,7 +157,7 @@ public class Main {
 
             }
         } else if (query instanceof Select) {
-            if(counter == 0) {
+            if(counter == 1) {
                 try {
                     sleep(10000);
                 } catch (InterruptedException e) {
